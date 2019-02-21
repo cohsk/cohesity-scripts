6b2d57614d051b2125b80977db37a6d054d8e393
Stephen Klosky <sklosky@gmail.com>
2019-02-20 19:55:52 -0800

    Update setupTA61.ps1

--- setupTA61.ps1
+++ setupTA61.ps1
@@ -31,14 +31,19 @@ Connect-CohesityCluster -Server 172.16.3.101 -Credential ($cred)
 #Register Remote Cluster 2
 Register-CohesityRemoteCluster -RemoteClusterIps 172.16.3.102 -RemoteClusterCredential ($cred) -EnableReplication -EnableRemoteAccess -StorageDomainPairs @{LocalStorageDomainId=$c1ndd.id;LocalStorageDomainName=“sd-ndd-ncc”;RemoteStorageDomainId=$c2ndd.id;RemoteStorageDomainName=“sd-ndd-ncc”}, @{LocalStorageDomainId=$c1idd.id;LocalStorageDomainName=”sd-idd-icc”;RemoteStorageDomainId=$c2idd.id;RemoteStorageDomainName="sd-idd-icc"}
 
+#Add Replication to the Bronze Protection Policy
+$pol = Get-CohesityProtectionPolicy -Names Bronze
+$rcluster = Get-CohesityRemoteCluster | Select ClusterId
+$target = New-Object 'Cohesity.Models.ReplicationTarget_' -ArgumentList $rcluster.clusterId, cohesity-02
+$reppol = New-Object 'Cohesity.Models.SnapshotReplicationCopyPolicy' -ArgumentList $true, 95, 1, KEvery, $target
+$pol.snapshotReplicationCopyPolicies = $reppol
+$pol | Set-CohesityProtectionPolicy
+
 Disconnect-CohesityCluster
 
-#Edit the Bronze Protection Policy and add replication
+#To be completed - Run the Physical Protection job
 
-#Run the Physical Protection job
-
-#Run the Virtual Protection job
-
+#To be completed - Run the Virtual Protection job
 
 ###Extra Credit — Run the BizApps Protection part of the lab
 
